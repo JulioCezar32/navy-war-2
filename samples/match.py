08167e439a8b5dfcfd240f6a61e4460cf6aa15a6
Julio Cezar <jc.2013oliveira@gmai.com>
2018-04-08 21:41:49 -0300

    Updating my files

--- samples/match.py
+++ samples/match.py
@@ -1,31 +1,50 @@
 class Match():
 
-    def __init__(self, ship_owner, bomb_owner):
+
+    def __init__(self, player_one, player_two):
         #receive all data from the match
-        self.ship_owner = ship_owner
-        self.bomb_owner = bomb_owner
-        self.ships = ship_owner._ships
-        self.bombs_positions = bomb_owner.bombs_position
-        self.place_bombs()
+        self.player_one = player_one
+        self.player_two = player_two
 
-    def place_bombs(self):
-        for ship in self.ships:
-            for bomb in self.bombs_positions:
-                if bomb in ship.positions:
-                    ship.structure[bomb] = 1
+    def place_bombs(self, player):
+        if self.player_one.name == player.name:
+            for ship in self.player_two._ships:
+                for bomb in self.player_one._bombs_positions:
+                    if bomb in self.player_two._ships_positions:
+                        ship.structure[bomb] = 1
 
-
+        elif self.player_two.name == player.name:
+            for ship in self.player_one._ships:
+                for bomb in self.player_two._bombs_positions:
+                    if bomb in ship.structure:
+                        ship.structure[bomb] = 1
         print("-------------------------")
         self.verify_ships_status()
 
+    def launch_bomb(self, player, bomb_position):
+
+        if self.player_one.name == player.name:
+            if bomb_position in self.player_one._bombs_positions:
+                print("Position {} Already Bombed".format(bomb_position))
+            else:
+                self.player_one._bombs_positions.append(bomb_position)
+            self.place_bombs(self.player_one)
+        elif self.player_two.name == player.name:
+            if bomb_position in self.player_two._bombs_positions:
+                print("Position {} Already Bombed".format(bomb_position))
+            else:
+                self.player_two._bombs_positions.append(bomb_position)
+            self.place_bombs(self.player_two)
+
     def verify_ships_status(self):
         #plot ship into the board
-        for ship in self.ships:
-            if all(ship.structure[position] == 1 for position in ship.positions):
-                print ("ship {} defeated".format(ship.name))
 
-            elif any(ship.structure[position] == 1 for position in ship.positions):
-                print ("ship {} damaged".format(ship.name))
+            for ship in self.player_one._ships:
+                if all(ship.structure[position] == 1 for position in ship.positions):
+                    print ("ship {} defeated".format(ship.name))
 
-            else:
-                print ("ship {} survived with no damage ".format(ship.name))
+                elif any(ship.structure[position] == 1 for position in ship.positions):
+                    print ("ship {} damaged".format(ship.name))
+
+                else:
+                    print ("ship {} survived with no damage ".format(ship.name))
--- samples/player.py
+++ samples/player.py
@@ -5,24 +5,17 @@ class Player():
         self._ships = []
         self._ships_positions = []
         self._board_position_validation = []
-        self._bombs_position = []
+        self._bombs_positions = []
 
     def create_ship(self, ship):
         ship_status = self.is_some_ship_overlaid(ship)
         if ship_status == 0:
             self._ships.append(ship)
-            self.ships_positions.append(ship.structure)
+            self._ships_positions.append(ship.structure)
             message = "Ship {} inserted correctly".format(ship.name)
         else:
             message = "The Ship {} was inserted into a wrong position".format(ship.name)
         print(message)
-
-    def launch_bomb(self, position):
-        if position in _self.bombs_position:
-            print("Position {} Already Bombed".format(position))
-        else:
-            self._bombs_position.append(position)
-
     def is_some_ship_overlaid(self, ship):
         if any(x in self._board_position_validation for x in ship.positions):
             return 1
